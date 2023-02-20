64434f1cc074eec45100b8f6b5f99fc3da83b866
SampleDummy <miloszwozny@poczta.onet.pl>
2023-02-20 22:35:39 +0100

    program

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+double derivative(double(double), double, double);
+
+int main()
+{
+    std::cout << derivative([](double x){return x * x;}, 2, 0.001) << std::endl;
+    return 0;
+}
+
+double derivative(double f(double), double x, double h)
+{
+    return (f(x + h) - f(x)) / h;
+}
